913ad8fe023c1f03640b64c0ae6fec9043def741
Lena Sabadina <elena.sabadina@icloud.com>
2019-09-24 14:36:33 +0200

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Lena Sabadina on 2019-09-24.
+//  Copyright © 2019 Whiskerz AB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
